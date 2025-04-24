6946b77d2be9c84c85988e8ed6882603fa5408e8
Armando <nania.armando@gmail.com>
2025-04-24 15:43:24 +0200

    Fix syntax parsing

--- get DNS server.py
+++ get DNS server.py
@@ -5,13 +5,24 @@ def get_dns_servers():
 	dns_servers = {}
 	current_iface = None
 
+	DNS_section = False
 	for line in result.stdout.splitlines():
-		if line.strip().endswith(':'):
-			current_iface = line.strip(':').strip()
+		if DNS_section:
+			if ": " in line:
+				DNS_section = False
+		
+
+		if DNS_section:
+			servers = line.split(':', 1)[1].strip().split()
+			for server in servers:
+				dns_servers[current_iface].append(server)
+		elif line.strip().endswith(')'):
+			current_iface = line.split("(")[1].split(")")[0]
 		elif 'DNS Servers' in line:
 			servers = line.split(':', 1)[1].strip().split()
 			if current_iface:
 				dns_servers[current_iface] = servers
+				DNS_section = True
 
 	return dns_servers
 
